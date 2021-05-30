d78d3581df7c605e85afd3a55d3bce48c89c9945
SloaneY <tpdms717@gmail.com>
2021-05-30 20:40:49 +0900

    card game

--- concentration.js
+++ concentration.js
@@ -107,3 +107,23 @@ function resetGame() {
   completed = [];
   startGame();
 }
+
+// 1분 퀴즈
+// function aaa() {
+//   setTimeout(() => {
+//     console.log("d");
+//   }, 0);
+//   console.log("c");
+// }
+
+// setTimeout(() => {
+//   console.log("a");
+//   aaa();
+// }, 0);
+
+// setTimeout(() => {
+//   aaa();
+//   console.log("b");
+// }, 0);
+
+// a c c b d d
